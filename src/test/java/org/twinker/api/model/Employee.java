67096a2dead8fde17e7e764dc8f13debb1159e59
Twinker <@example.com>
2025-04-21 16:57:58 +0200

    fixed gradle run

--- src/test/java/org/twinker/api/model/Employee.java
+++ src/test/java/org/twinker/api/model/Employee.java
@@ -1,9 +1,21 @@
-//package org.twinker.api.model;
-//
-//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-//import lombok.Builder;
-//
-//@Builder
-//@JsonIgnoreProperties(ignoreUnknown = true)
-//public record Employee(int id, Boolean isActive, String firstName, String lastName, String phone, String email) {
-//}
+package org.twinker.api.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    private int id;
+    private Boolean isActive;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+}
